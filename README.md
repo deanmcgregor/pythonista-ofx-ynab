c1c17452967c7eb530b131c6e16de4ea892a05f6
Dean McGregor <deanm@synopsys.com>
2018-05-12 16:43:15 +1000

    Update to read OFX instead of CSV.
    This should be more stable than CSV files for other banks. It also makes sharing it easier from iOS since Safari doesn't open OFX files like it does CSV.

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# Commmbank/ING Import Script for YNAB
+# OFX Import Script for YNAB
 
-This script imports CSV files from ING Australia and the Commonwealth Bank to YNAB using their API. 
+This script imports OFX files to YNAB using their (currently private) API. 
+
+This script has been tested with OFX files from ING Australia and the Commonwealth Bank. They both have some incompatabilities with the ofxtools library I've used, so there's some munging of the XML to make it work. Not tested for other banks, YMMV.
 
 It is designed to be used with Pythonista on iOS. The script is set up so that you can use the iOS sharing interface with Pythonista to call this script.
 That should allow you to grab a CSV from the web and auto-import it into YNAB.
--- import_to_ynab.py
+++ import_to_ynab.py
@@ -4,9 +4,87 @@ import dialogs
 import sys
 import csv
 from datetime import datetime
-
+from ofxtools.Parser import OFXTree
+import xml.etree.ElementTree as ET 
+import tempfile
 import ynab
 
+def read_ofx_from_file(file_path):
+    # ING thinks it's a good idea to embed HTML inside XML with no escaping
+    # This strips that out.
+    ofx_file = open(file_path, 'r')
+    ofx_file_content = ofx_file.read().replace("<BR/>", " ")
+    ofx_file.close()
+
+    with tempfile.NamedTemporaryFile() as temp:
+        temp.write(ofx_file_content)
+        temp.seek(0)
+
+        parser = OFXTree()
+        parser.parse(temp)
+
+        # need this for a few fields later
+        now = datetime.now().date()
+        ofx_now_string = now.strftime("%Y%m%d")
+        epoch_string = "19700101"
+
+        #check acctfrom exists, if not make it
+        #we can use a dummy value since don't need this field in our import
+        stmtr = parser.find(".//STMTRS")
+        if stmtr is None:
+            print "Cannot find STMTR"
+            exit(1) 
+
+        acctfrom = parser.find(".//STMTRS/BANKACCTFROM")
+        if acctfrom is None:
+            acctfrom_tree = ET.fromstring("<BANKACCTFROM><BANKID>0</BANKID><ACCTID>0</ACCTID><ACCTTYPE>SAVINGS</ACCTTYPE></BANKACCTFROM>")
+
+            stmtr.insert(0, acctfrom_tree)
+
+        # We also ignore this, so just put some dummy values
+        ledger = parser.find(".//STMTRS/LEDGERBAL")
+        if ledger is not None:
+            stmtr.remove(ledger)
+        ledger_tree = ET.fromstring("<LEDGERBAL><BALAMT>0</BALAMT><DTASOF>"+ofx_now_string+"</DTASOF></LEDGERBAL>")        
+        stmtr.insert(0,ledger_tree)
+
+        avail = parser.find(".//STMTRS/AVAILBAL")
+        if avail is not None:
+            stmtr.remove(avail)
+        avail_tree = ET.fromstring("<AVAILBAL><BALAMT>0</BALAMT><DTASOF>"+ofx_now_string+"</DTASOF></AVAILBAL>")        
+        stmtr.insert(0,avail_tree) 
+
+        # Again not really used with our import, so just set the maximum time frame
+        banktranlist = parser.find(".//BANKTRANLIST")
+        if banktranlist is None:
+            print "Cannot find BANKTRANLIST"
+            exit(1) 
+
+        dtstart = parser.find(".//BANKTRANLIST/DTSTART")
+        if dtstart is None:
+            
+            dtstart_tree = ET.fromstring("<DTSTART>"+epoch_string+"</DTSTART>")
+            dtend_tree = ET.fromstring("<DTEND>"+ofx_now_string+"</DTEND>")
+
+            banktranlist.insert(0, dtstart_tree)
+            banktranlist.insert(1, dtend_tree)
+
+        # Commbanks fault this time. They have some non-standard date here, at least for this library
+        # This standardises them to be the same
+        sors = parser.find(".//SONRS")
+        if sors is None:
+            print "Cannot find SONRS"
+            exit(1)
+
+        dtserver = parser.find(".//SONRS/DTSERVER")
+        if dtserver is not None:
+            sors.remove(dtserver)
+
+        dtserver_val = ET.fromstring("<DTSERVER>"+ofx_now_string+"</DTSERVER>")
+        sors.insert(0, dtserver_val)
+
+        return parser.convert()
+ 
 def get_account(account_name, api_client, budget):
   try:
     for a in accounts_response.data.accounts:
@@ -17,9 +95,9 @@ def get_account(account_name, api_client, budget):
     print("When trying to get accounts, encounted the error %s" % e)
 
 if appex.is_running_extension():
-  csv_file = appex.get_file_path()
+  ofx_file = appex.get_file_path()
 else:
-  csv_file = sys.argv[1]
+  ofx_file = sys.argv[1]
 
 key_file = open("key.txt", 'r')
 key = key_file.readline().strip()
@@ -72,57 +150,48 @@ import_id_table = {}
 
 transaction_list = []
 
+ofx = read_ofx_from_file(ofx_file)
 
-with open(csv_file, 'r') as f:
-  #Commbank have CSV files with no headers...
-  #Check the first line to see if it's ING or Commbank's CSV file
-  first_line = f.readline()
-  f.seek(0) #rewind
-  if first_line.startswith("Date,"):
-    c = csv.DictReader(f)
-  else:
-    c = csv.DictReader(f, fieldnames=["Date", "Debit", "Description", "Balance", "Category"])
+for record in ofx.statements[0].transactions:
 
-  for record in c:
-    
     #Commbank only has one column to show transactions,
     #ING has two. Below allows for this discrepency
-    amount = record["Debit"]
-    if amount == "":
-      amount = record["Credit"]
+    amount = record.trnamt
     amount = int(float(amount)*1000) #This is how the YNAB API repersents money
+
     imported_date = datetime.now().date()
-    date = record["Date"]
-    
-    transaction_date = datetime.strptime(date, "%d/%m/%Y").date()
-    
+
+    date = record.dtposted
+
+    transaction_date = date.date()
+
     date_str = transaction_date.isoformat()
-    
+
     transaction_diff = imported_date - transaction_date
-    
+
     # We only care about the last 30 days
     if transaction_diff.days > 30:
-    	break
-    
-    description = record["Description"]
+        break
+
+    description = record.memo
     payee_name = description.split("- ")[0]
-    
+
     import_id_candidate = "YNAB:"+(str(amount))+":"+date_str
     if import_id_candidate in import_id_table:
-      num = import_id_table[import_id_candidate]
-      import_id_table[import_id_candidate] += 1
+        num = import_id_table[import_id_candidate]
+        import_id_table[import_id_candidate] += 1
     else:
-      import_id_table[import_id_candidate] = 1
-      num = 1
-    
+        import_id_table[import_id_candidate] = 1
+        num = 1
+
     import_id = import_id_candidate+":"+str(num)
-    
+
     transaction = ynab.SaveTransaction(account_id=this_account, 
-                                       date=date_str, 
-                                       amount=amount, 
-                                       payee_name=payee_name,
-                                       import_id=import_id)
-    
+                                        date=date_str, 
+                                        amount=amount, 
+                                        payee_name=payee_name,
+                                        import_id=import_id)
+
     transaction_list.append(transaction)
 
 try:
